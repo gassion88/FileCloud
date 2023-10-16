c3bd2ba160725802f3dd97cec1641168ace2f432
Soslan Gassiev <soslangassity04@gmail.com>
2023-10-16 12:14:02 +0300

    init :

---
+++ src/main/java/com/gassion/filecloudbackend/FileCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gassion.filecloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCloudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCloudBackendApplication.class, args);
+    }
+
+}
