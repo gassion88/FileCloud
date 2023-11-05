81b2f92a93588ee4961df6121fdcfe9215b265c6
Soslan Gassiev <soslangassity04@gmail.com>
2023-11-05 16:40:33 +0300

    feat : add mapper

---
+++ src/main/java/com/gassion/filecloudbackend/storage/mapper/BucketItemToUserBucketResponseMapper.java
@@ -0,0 +1,10 @@
+package com.gassion.filecloudbackend.storage.mapper;
+
+import com.gassion.filecloudbackend.common.mapper.Mapper;
+import com.gassion.filecloudbackend.storage.web.UserBucketResponse;
+import io.minio.messages.Item;
+
+import java.util.List;
+
+public interface BucketItemToUserBucketResponseMapper extends Mapper<UserBucketResponse, List<Item>> {
+}
---
+++ src/main/java/com/gassion/filecloudbackend/storage/mapper/impl/BucketItemToUserBucketResponseMapperImpl.java
@@ -0,0 +1,29 @@
+package com.gassion.filecloudbackend.storage.mapper.impl;
+
+import com.gassion.filecloudbackend.storage.mapper.BucketItemToUserBucketResponseMapper;
+import com.gassion.filecloudbackend.storage.model.BucketFile;
+import com.gassion.filecloudbackend.storage.web.UserBucketResponse;
+import io.minio.messages.Item;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BucketItemToUserBucketResponseMapperImpl implements BucketItemToUserBucketResponseMapper {
+
+    @Override
+    public UserBucketResponse map(List<Item> source) {
+        UserBucketResponse userBucketResponse = new UserBucketResponse(new ArrayList<>());
+
+        for (Item item : source) {
+            BucketFile bucketFile = new BucketFile(item.objectName(),
+                    item.size(),
+                    item.lastModified());
+
+            userBucketResponse.userFiles().add(bucketFile);
+        }
+
+        return userBucketResponse;
+    }
+}
